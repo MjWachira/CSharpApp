976b0e26aedff844193956b9224475cd524c09aa
Johnny <mjwachira1@gmail.com>
2023-11-21 14:09:01 +0300

    Add project files.

---
+++ testApp/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+string name = "John";
+
+Console.WriteLine(name);
+
+int a = 10;
+int b  = 20;
+
+int sum = a + b;
+
+
+Console.WriteLine(sum);
+
+double c = 654 / 233;
+
+Console.WriteLine(c);
+
+if(a == b)
+{
+    Console.WriteLine("numbers match");
+}
+else
+{
+    Console.WriteLine("numbers dont match");
+}
